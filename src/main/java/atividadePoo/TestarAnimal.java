09d3b533bea9cc3f79aa9fd7c84280c707719669
JairAssisDev <jairv141@gmail.com>
2023-04-06 10:58:41 -0300

    atz

--- src/main/java/atividadePoo/TestarAnimal.java
+++ src/main/java/atividadePoo/TestarAnimal.java
@@ -2,7 +2,7 @@ package atividadePoo;
 
 public class TestarAnimal {
     public static void main(String[] args) {
-        Mamifero camelo = new Mamifero("Camelo", 300.0, 3, "meio amarelado cor de caramelo", "Terra",3.0, "plantas");
+        Mamifero camelo = new Mamifero("Camelo", 300.0, 3, "meio amarelado cor de caramelo", "Terra",3.0, "plantas e mama");
         Peixe tubarao = new Peixe("Tubarão", 250.0, 0, "branca", "Mar", 6.5, "Barbatanas e cauda");
         
         System.out.println("lista de comfiguraçoes do camelinho");
