260ac3e1b2e1ce7ba09c907527275c8ca76c6b0c
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-13 19:38:46 -0300

    Add files via upload

--- bytebank/Conta.java
+++ bytebank/Conta.java
@@ -1,38 +1,8 @@
 package bytebank;
 
 public class Conta {
-	private double saldo;
-	private int agencia;
-	private int numero;
-	private String titular;
-	
-	public void mostraConta() {
-		System.out.println(getTitular()+" "
-		+getSaldo()+" "+getAgencia()+" "+getNumero());
-	}
-	public double getSaldo() {
-		return saldo;
-	}
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	public int getAgencia() {
-		return agencia;
-	}
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
-	public int getNumero() {
-		return numero;
-	}
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	public String getTitular() {
-		return titular;
-	}
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
-	
+    double saldo;
+    int agencia = 42;
+    int numero;
+    String titular;
 }
