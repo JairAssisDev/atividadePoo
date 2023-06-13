ee2668b976d9724df7416c27a54b8e1ca8c9884f
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-13 19:38:59 -0300

    Add files via upload

--- bytebank/CriaConta.java
+++ bytebank/CriaConta.java
@@ -1,15 +1,36 @@
 package bytebank;
 
-public class CriaConta {
-	public static void main(String[] args) {
-		
-		Conta conta1 = new Conta();
-		conta1.setAgencia(1233);
-		conta1.setNumero(233);
-		conta1.setSaldo(2333.8);
-		conta1.setTitular("jair");
-		
-		conta1.mostraConta();
-	
-	}
+public class CriaConta { 
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200; 
+        System.out.println(primeiraConta.saldo); 
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 300;
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        segundaConta.agencia = 146;
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+            System.out.println(segundaConta.agencia);
+
+        segundaConta.agencia = 146;
+        System.out.println("agora a segunda conta está na agencia " + segundaConta.agencia);
+
+        if(primeiraConta == segundaConta) {
+            System.out.println("mesma conta");
+        } else {
+            System.out.println("contas diferentes");
+        }
+
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
+    }
 }
