a3b814ab296403bd5af18fba8abc6ba1df8b35c7
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-16 09:39:04 -0300

    Add files via upload

--- bytebank/Conta.java
+++ bytebank/Conta.java
@@ -9,9 +9,25 @@ public class Conta {
     public void depositar(double saldoQueVaiSerDepositado) {
     	this.saldo+=saldoQueVaiSerDepositado;
     }
-    public void sacar(double sacarValor) {
+    public boolean sacar(double sacarValor) {
+    	if(this.saldo>=sacarValor) {
     	this.saldo-=sacarValor;
     	System.out.println("Você tirou: "+ sacarValor);
     	System.out.println("Seu saldo agora é: "+this.saldo);
+    	return true;
+    	}
+			return false;
+		
+    	
+    }
+    public boolean transfere(double valor, Conta destino) {
+    	if(this.saldo>=valor) {
+    		this.saldo-=valor;
+    		destino.depositar(valor);
+    		return true;
+    	}
+    		return false;
+    	
+    	
     }
 }
--- bytebank/TestaMetodo.java
+++ bytebank/TestaMetodo.java
@@ -8,14 +8,30 @@ public class TestaMetodo {
 		conta.agencia= 2323;
 		conta.numero=123;
 		conta.titular="jair";
-		conta.saldo=1000;
+		conta.saldo=1000.0;
 		
+		Conta conta2 = new Conta();
+		conta2.agencia=2321;
+		conta2.numero=232;
+		conta2.titular="paulo";
+		conta2.saldo=300.0;
 		System.out.println("saldo do "+conta.titular
 				+" Saldo : "+conta.saldo);
 		conta.depositar(233.70);
 		System.out.println("saldo do "+conta.titular
 				+" Saldo : "+conta.saldo);
-		conta.sacar(200.0);
+		boolean conseguiuSacar = conta.sacar(200.0);
+		System.out.println(conseguiuSacar);
+		
+		System.out.println(conta2.saldo);
+		boolean transfenciaComSucesso=conta.transfere(4000.0, conta2);
+		if(transfenciaComSucesso) {
+			System.out.println("transferencia com sucesso");
+		}else {
+			System.out.println("faltou dinheiro");
+		}
+		
+		
 	}
 
 }
