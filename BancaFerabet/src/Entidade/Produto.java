9643e368d5ceb357768723143b04c557ccf833df
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-06-18 17:20:15 -0300

    Add files via upload

---
+++ BancaFerabet/src/Entidade/Produto.java
@@ -0,0 +1,58 @@
+package Entidade;
+
+public abstract class Produto {
+	private int codigo;
+	private String nome;
+	private int estoque;
+	private double valorUnitario;
+	
+	public Produto(int codigo, String nome) {
+		this.codigo= codigo;
+		this.nome=nome;
+		
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	public void incluirEstoque(int quantidade) {
+		estoque = estoque + quantidade;
+	}
+	public void tirarEstoque(int quantidade) {
+		if (quantidade<=estoque) {
+			estoque = estoque - quantidade;
+		}else {
+			System.out.println("A quantidade que vc deseja nao costa no estoque:");
+			System.out.println("----------------");
+			
+		}
+	}
+}
