15b2ac4c24f9f242bee98f3b5d3d12c0115cad37
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-07-27 22:28:33 -0300

    Add files via upload

---
+++ IFPE/src/main/java/br/com/palmares/IFPE/IfpeApplication.java
@@ -0,0 +1,13 @@
+package br.com.palmares.IFPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfpeApplication.class, args);
+	}
+
+}
---
+++ IFPE/src/test/java/br/com/palmares/IFPE/IfpeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.palmares.IFPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
